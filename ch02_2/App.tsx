1c2acd8e62b7d6f9521d0d0bb74a847d853fd7f3
nadajinny <jinsun23007@gmail.com>
2025-05-09 05:53:14 -0700

    조건문을 단축 평가 코드로 바꿔 해결

--- ch02_2/App.tsx
+++ ch02_2/App.tsx
@@ -3,11 +3,10 @@ import { SafeAreaView, Text } from "react-native"
 
 export default function App() {
   const isLoading = true
-  if(isLoading) {
-    return (
-      <SafeAreaView>
-        <Text>Loading...</Text>
-      </SafeAreaView>
-    )
-  }
+  return (
+    <SafeAreaView>
+      {isLoading && <Text>Loading... </Text>}
+      {!isLoading && <Text>Hello JSX world!</Text>}
+    </SafeAreaView>
+  )
 }
