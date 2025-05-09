14d9b088cb54f07f102cb04261afc73595a1f2d1
nadajinny <jinsun23007@gmail.com>
2025-05-09 05:11:16 -0700

    console.log 사용

--- ch02_1/App.tsx
+++ ch02_1/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { View, Text } from 'react-native'
 
 export default function App() {
+  console.log('App Called')
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Hello World!</Text>
