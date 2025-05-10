a6b1da6e78de8384a5f8828a245284297a44f572
nadajinny <jinsun23007@gmail.com>
2025-05-10 03:39:38 -0700

    border 관련 속성 추가하기

--- ch03_2/App.tsx
+++ ch03_2/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { Platform, Dimensions, StyleSheet, SafeAreaView, Text } from "react-native";
-import {MD3Colors} from 'react-native-paper'; 
+import { Platform, Dimensions, StyleSheet, SafeAreaView, Text, View} from "react-native";
 
 const {width, height} = Dimensions.get('window')
-const blue500 = '#2196f3'
-const blue200 = '#90caf9'
+
+const BLUE_200 = "#90caf9";
+const BLUE_500 = "#2196f3";
+const BLUE_900 = "#0d47a1";
+const LIME_500 = "#cddc39";
 
 export default function App() {
   return (
@@ -12,11 +14,17 @@ export default function App() {
       <Text style = {[styles.text]}>os: {Platform.OS}</Text>
       <Text style = {[styles.text]}>width: {width}px</Text>
       <Text style = {[styles.text]}>height: {height}px</Text>
+
+      <View style = {[styles.box]}/>
+      <View style = {[styles.box, styles.border, {borderRadius: 20}]}/>
+      <View style = {[styles.box, styles.border, {borderTopLeftRadius: 40, borderBottomLeftRadius: 40}]}/>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  SafeAreaView: {backgroundColor: blue500}, 
-  text: {fontSize: 20, color: blue200}
+  SafeAreaView: {backgroundColor: BLUE_500}, 
+  text: {fontSize: 20, color: BLUE_200},
+  box: {height: 100, backgroundColor: LIME_500, marginBottom: 10}, 
+  border: {borderWidth: 10, borderColor: BLUE_900}
 })
