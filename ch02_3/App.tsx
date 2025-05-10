bd05e419e266cda7d932d3d48cec8f884707ffab
nadajinny <jinsun23007@gmail.com>
2025-05-09 22:20:57 -0700

    클래스 컴포넌트를 활용하기

--- ch02_3/App.tsx
+++ ch02_3/App.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import { SafeAreaView, Text } from "react-native";
-import * as D from './src/data'
+import ClassComponent from "./src/screens/Classcomponent";
 
-const person = D.createRandomPerson()
 export default function App() {
   return (
-    <SafeAreaView>
-      <Text>{JSON.stringify(person, null, 2)}</Text>
-    </SafeAreaView>
+    <SafeAreaView><ClassComponent/></SafeAreaView>
   )
 }
