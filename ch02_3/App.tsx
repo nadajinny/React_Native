d5024cd1b1b2809c71b6f4ed350509211b424207
nadajinny <jinsun23007@gmail.com>
2025-05-09 22:44:15 -0700

    ScrollView 코어 컴포넌드와 key 속성 활용하기

--- ch02_3/App.tsx
+++ ch02_3/App.tsx
@@ -1,14 +1,18 @@
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, Text , ScrollView} from "react-native";
 import ClassComponent from "./src/screens/Classcomponent";
 import ArrowComponent from "./src/screens/ArrowComponent";
 import Person from "./src/screens/Person";
 import * as D from './src/data'
 
-const person = D.createRandomPerson()
+const people = D.makeArray(100).map(D.createRandomPerson)
 
 export default function App() {
+  const children= people.map((person)=>(<Person key = {person.id} person= {person}/>))
+
   return (
-    <SafeAreaView><ClassComponent/><ArrowComponent/><Person person = {person}/></SafeAreaView>
+    <SafeAreaView>
+      <ScrollView>{children}</ScrollView>
+    </SafeAreaView>
   )
 }
