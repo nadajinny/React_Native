e29db67ffdd75d0b43d16c6c83f1dbf7c8bc0991
nadajinny <jinsun23007@gmail.com>
2025-05-09 22:27:08 -0700

    화살표 방식 컴포넌트를 활용하기

--- ch02_3/App.tsx
+++ ch02_3/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { SafeAreaView, Text } from "react-native";
 import ClassComponent from "./src/screens/Classcomponent";
+import ArrowComponent from "./src/screens/ArrowComponent";
 
 export default function App() {
   return (
-    <SafeAreaView><ClassComponent/></SafeAreaView>
+    <SafeAreaView><ClassComponent/><ArrowComponent/></SafeAreaView>
   )
 }
