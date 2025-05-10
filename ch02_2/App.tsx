89a1393c46124ad25593f885c4b9c8b8662f6b00
nadajinny <jinsun23007@gmail.com>
2025-05-09 21:03:06 -0700

    조금씩 다른 100개의 컴포넌트 배열 만들기

--- ch02_2/App.tsx
+++ ch02_2/App.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { SafeAreaView, Text } from "react-native"
 
 export default function App() {
-  const children = [1,2,3].map((i)=><Text>Hello World!{i}</Text>)
-
+  const children = new Array(100).fill(null)
+    .map((notUsed, index)=><Text>Hello World!{index}</Text>)
   return <SafeAreaView>{children}</SafeAreaView>
 }
