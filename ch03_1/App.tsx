0679f0b79919a1d670a566ec2dce28c30749a617
nadajinny <jinsun23007@gmail.com>
2025-05-10 02:28:22 -0700

    Colors의 blue500을 활용하려 했지만 존재하지않아 정의 후 사용

--- ch03_1/App.tsx
+++ ch03_1/App.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { StyleSheet, SafeAreaView, Text } from "react-native";
+import { MD3Colors } from "react-native-paper";
+
+const blue500 = '#2196f3'
+const blue200 = '#90caf9'
+
+console.log(blue500)
 
 export default function App() {
   return (
-    <SafeAreaView style = {[styles.safeAreaView, {backgroundColor: 'blue'}]}>
-      <Text style = {[styles.text, {color: 'white'}]}>
-        Hello StyleSheet world!
-      </Text>
+    <SafeAreaView style= {[styles.safeAreaView]}>
+      <Text style={[styles.text]}>Hello StyleSheet world!</Text>
     </SafeAreaView>
   )
 }
+
+
 const styles = StyleSheet.create({
-  safeAreaView: {flex: 1, alignItems: 'center', justifyContent: 'center'},
-  text: {fontSize: 20}
+  safeAreaView: {flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: blue500},
+  text: {fontSize: 20, color: blue200}
 })
