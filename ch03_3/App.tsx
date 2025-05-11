49e5c4af1be6213526694b03c8cdee78de3f7a6b
nadajinny <jinsun23007@gmail.com>
2025-05-11 01:27:30 -0700

    폰트 자원 활용

--- ch03_3/App.tsx
+++ ch03_3/App.tsx
@@ -1,14 +1,36 @@
 import React from "react";
-import { StyleSheet, SafeAreaView, ImageBackground } from "react-native";
+import { StyleSheet, SafeAreaView, ImageBackground, Image, View, Text, Platform} from "react-native";
+import * as D from './src/data'
+
+const avatarUrl = D.randomAvatarUrl()
+const avatarSize = 50
+const text = 'Almost before we knew it, we had left the ground. '
 
 export default function App() {
   return (
     <SafeAreaView style = {[styles.flex]}>
-      <ImageBackground style = {[styles.flex]} source = {require('./src/assets/images/image.png')}/>
+      <ImageBackground style = {[styles.flex]} source = {require('./src/assets/images/image.png')}>
+        <Image source = {{uri: avatarUrl}} style= {[styles.image]}/>
+        <View style={[styles.flex, styles.padding10]}> 
+          <Text style={[styles.text, styles.regular]}>{text}[regular]</Text>
+          <Text style={[styles.text, styles.medium]}>{text}[medium]</Text>
+          <Text style={[styles.text, styles.semiBold]}>{text}[semiBold]</Text>
+          <Text style={[styles.text, styles.bold]}>{text}[bold]</Text>
+        </View>
+      </ImageBackground>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  flex: {flex: 1}
+  flex: {flex: 1},
+  ImageBackground: {padding: 10}, 
+  image: {width: avatarSize, height: avatarSize, borderRadius: avatarSize/2},
+  padding10: {padding: 10}, 
+  text: {textAlign: 'center', fontSize: 25, color: 'white', marginBottom: 10}, 
+
+  regular: {fontFamily: 'DancingScript-Regular', fontWeight: '400'}, 
+  medium: {fontFamily: 'DancingScript-Medium', fontWeight: '500'}, 
+  semiBold: {fontFamily: 'DancingScript-SemiBold', fontWeight: '600'}, 
+  bold: {fontFamily: 'DancingScript-Bold', fontWeight: Platform.select({ios: '700', android: '600'})}
 })
