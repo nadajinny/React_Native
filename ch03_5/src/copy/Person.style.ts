63382478fb6499572892902a374d8fa71b4b3532
nadajinny <jinsun23007@gmail.com>
2025-05-12 05:33:16 -0700

    Person 컴포넌트 초기 스타일

--- ch03_5/src/copy/Person.style.ts
+++ ch03_5/src/copy/Person.style.ts
@@ -1,3 +1,17 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from "react-native"
 
-export const styles = StyleSheet.create({})
+const lime500 = '#CDDC39'
+
+export const styles = StyleSheet.create({
+    view: { backgroundColor: lime500, padding: 5 }, 
+    avatar: { width: 50, height: 50, borderRadius: 25}, 
+    nameEmailView: { flexDirection: 'row', alignItems: 'center'}, 
+    name: { marginRight: 5, fontSize: 22, fontWeight: '500'}, 
+    email: {}, 
+    dateView: {}, 
+    createdDate: {}, 
+    text: {}, 
+    image: { width: '100%', height: 150 }, 
+    countsView: {flexDirection: 'row', padding: 3, justifyContent: 'space-around'}, 
+    counts: {} 
+})
