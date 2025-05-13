c377bce68b89814c4bb8423ab45d8b2e6c285b50
nadajinny <jinsun23007@gmail.com>
2025-05-13 02:30:00 -0700

    FC타입이 children 속성을 제공하는 것에 착안한 구현

--- ch03_5/src/components/TouchableView.tsx
+++ ch03_5/src/components/TouchableView.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import type {FC, ReactNode, ComponentProps} from 'react'
 import { TouchableOpacity, View } from "react-native";
 
-export type TouchableViewProps = { children?: ReactNode }
+type TouchableOpacityProps = ComponentProps<typeof TouchableOpacity>
+
+export type TouchableViewProps = TouchableOpacityProps
 
 export const TouchableView: FC<TouchableViewProps> = ({children, ...touchableProps}) => {
     return (<TouchableOpacity {...touchableProps}>
