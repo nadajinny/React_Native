4c84662cdde6024a738075b8e0f06ca4cffd0250
nadajinny <jinsun23007@gmail.com>
2025-05-13 02:26:44 -0700

    타입스크립트 교집합 타입과 JSX 전개 연산자 구문 혼합 사용

--- ch03_5/src/components/TouchableView.tsx
+++ ch03_5/src/components/TouchableView.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import type {FC, ReactNode} from 'react'
+import type {FC, ReactNode, ComponentProps} from 'react'
 import { TouchableOpacity, View } from "react-native";
 
 export type TouchableViewProps = { children?: ReactNode }
 
-export const TouchableView: FC<TouchableViewProps> = ({children}) => {
-    return (<TouchableOpacity>
+export const TouchableView: FC<TouchableViewProps> = ({children, ...touchableProps}) => {
+    return (<TouchableOpacity {...touchableProps}>
         <View>{children}</View>
     </TouchableOpacity>)
 }
