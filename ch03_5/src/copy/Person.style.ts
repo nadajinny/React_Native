1e0be10cf62d11e016e1e8d850d432c46773c2c6
nadajinny <jinsun23007@gmail.com>
2025-05-14 01:58:48 -0700

    Avatoar,Icontext컴포넌트를 person컴포넌트에 적용

--- ch03_5/src/copy/Person.style.ts
+++ ch03_5/src/copy/Person.style.ts
@@ -1,17 +1,25 @@
-import { StyleSheet } from "react-native"
+import { StyleSheet } from "react-native";
 
-const lime500 = '#CDDC39'
+const lime100 = '#F0F4C3';
+const blue500 = '#2196F3'; 
+const deepPurple500 = '#673AB7'; 
 
 export const styles = StyleSheet.create({
-    view: { backgroundColor: lime500, padding: 5 }, 
-    avatar: { width: 50, height: 50, borderRadius: 25}, 
-    nameEmailView: { flexDirection: 'row', alignItems: 'center'}, 
-    name: { marginRight: 5, fontSize: 22, fontWeight: '500'}, 
-    email: {}, 
-    dateView: {}, 
-    createdDate: {}, 
-    text: {}, 
-    image: { width: '100%', height: 150 }, 
+    view: {flexDirection: 'row', backgroundColor: lime100, padding: 5}, 
+    leftView: {padding : 5}, 
+    avatar: {borderColor: blue500, borderWidth: 2}, 
+    rightView: {flex: 1, padding: 5, marginRight: 10}, 
+    name: {marginRight: 5, fontSize: 22, fontWeight: '500'}, 
+    email: {
+        textDecorationLine: 'underline', color: blue500, textDecorationColor: blue500
+    }, 
+    dataView: {
+        flexDirection: 'row', justifyContent: 'space-between', padding: 3, marginTop: 5 
+    }, 
+    text: {fontSize: 16}, 
+    comments: {marginTop: 10, fontSize: 16}, 
+    image: {height: 150, marginTop: 15}, 
     countsView: {flexDirection: 'row', padding: 3, justifyContent: 'space-around'}, 
-    counts: {} 
+    touchableIcon: {flexDirection: 'row', padding: 3, alignItems: 'center'},
+    iconText: {color: deepPurple500, marginLeft: 3}
 })
--- ch03_5/src/copy/Person.tsx
+++ ch03_5/src/copy/Person.tsx
@@ -1,33 +1,47 @@
 import React from "react";
 import type { FC } from "react";
-import {View, Text, Image} from 'react-native'
-import { styles } from "./Person.style";
-import * as D from '../data'
+import {View, Text, Image, Alert} from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import moment from 'moment-with-locales-es6'
+import * as D from '../data'
+import { Avatar, IconText } from "../components";
+import {styles} from './Person.style'
 
-moment.locale('ko')
+moment.locale('ko') 
 
-export type PersonProps = {    person : D.IPerson  }
+export type PersonProps = { person : D.IPerson }
+const lightBlue500 = '#03A9F4'; 
+const blue500 = '#2196F3'; 
+const purple500 = '#9C27B0';
+const red500 = '#F44336'; 
+const avatarPressed = () => Alert.alert('avatar pressed')
+const deletePressed = () => Alert.alert('delete pressed')
+const countIconPressed = (name: string) => () => Alert.alert(`${name} pressed`)
 
 const Person: FC<PersonProps> = ({person}) => {
     return (
         <View style = {[styles.view]}>
-            <Image source={{uri: person.avatar}} style= {[styles.avatar]} />
-            <View style={[styles.nameEmailView]}>
-                <Text style={[styles.name]}>{person.name}</Text>
+            <View style = {[styles.leftView]}>
+                <Avatar imageStyle={[styles.avatar]} uri = {person.avatar} size = {50} onPress={avatarPressed}/>
+            </View>
+            <View style={[styles.rightView]}>
+                <Text style={[styles.name]}>{person.name}</Text>    
                 <Text style={[styles.email]}>{person.email}</Text>  
-            </View>
-            <View style={[styles.dateView]}>
-                <Text style={[styles.createdDate]}>
-                    {moment(person.createdDate).startOf('day').fromNow()}
+                <View style={[styles.dataView]}>
+                    <Text style={[styles.text]}>
+                        {moment(person.createdDate).startOf('day').fromNow()}
+                    </Text>
+                    <Icon name='trash-can-outline' size={26} color={lightBlue500} onPress={deletePressed}/>
+                </View>
+                <Text numberOfLines={3} ellipsizeMode="tail" style={[styles.text, styles.comments]}>
+                    {person.comments}
                 </Text>
-            </View>
-            <Text style={[styles.text]}>{person.comments}</Text>
-            <Image style={[styles.image]} source={{uri: person.image}}/>
-            <View style={[styles.countsView]}>
-                <Text style={[styles.counts]}>{person.counts.comment}</Text>
-                <Text style={[styles.counts]}>{person.counts.retweet}</Text>
-                <Text style={[styles.counts]}>{person.counts.heart}</Text>
+                <Image style={[styles.image]} source={{uri: person.image}}/>
+                <View style={[styles.countsView]}>
+                    <IconText viewStyle={[styles.touchableIcon]} onPress={countIconPressed('comment')} name="comment" size = {24} color={blue500} textStyle={[styles.iconText]} text={person.counts.comment}/>
+                    <IconText viewStyle={[styles.touchableIcon]} onPress={countIconPressed('retweet')} name="twitter-retweet" size = {24} color={purple500} textStyle={[styles.iconText]} text={person.counts.retweet}/>
+                    <IconText viewStyle={[styles.touchableIcon]} onPress={countIconPressed('heart')} name="heart" size = {24} color={red500} textStyle={[styles.iconText]} text={person.counts.heart}/>
+                </View>
             </View>
         </View>
     )
